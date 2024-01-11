e77107de1ef7b5645d15f8bdcb28b51e8d162cf3
Koech <koech@novek.io>
2024-01-11 12:27:38 +0300

    (f/project1/add-qt-6-support): added clang-format and removed QBS build script

--- 1.Project1/main.cpp
+++ 1.Project1/main.cpp
@@ -4,14 +4,13 @@
 int main(int argc, char *argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
-    QGuiApplication app(argc, argv);
+  QGuiApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-    if (engine.rootObjects().isEmpty())
-        return -1;
+  QQmlApplicationEngine engine;
+  engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+  if (engine.rootObjects().isEmpty()) return -1;
 
-    return app.exec();
+  return app.exec();
 }
